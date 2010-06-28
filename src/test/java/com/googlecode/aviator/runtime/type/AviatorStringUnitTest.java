4a4a25cfa1c115b560d804aa167d9ab09685199d
killme2008 <killme2008@02685280-edcf-5368-d9ee-a7af13fa1820>
2010-06-28 07:03:26 +0000

    添加LGPL协议声明。

--- src/test/java/com/googlecode/aviator/runtime/type/AviatorStringUnitTest.java
+++ src/test/java/com/googlecode/aviator/runtime/type/AviatorStringUnitTest.java
@@ -1,3 +1,27 @@
+/**
+ *  Copyright (C) 2010 dennis zhuang (killmimport static org.junit.Assert.*;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Test;
+
+import com.googlecode.aviator.exception.ExpressionRuntimeException;
+icense, or
+ *  (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ **/
 package com.googlecode.aviator.runtime.type;
 
 import static org.junit.Assert.*;
